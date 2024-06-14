03e2b4c531f0a16ab686e683e15e3a459adc1ebc
Marc Singleton <marcsingleton@berkeley.edu>
2024-06-13 23:14:22 -0700

    Updated count_words.py to deal with punctuation

--- code/count_words.py
+++ code/count_words.py
@@ -1,9 +1,24 @@
 """Count words in a file."""
 
 import os
+import re
 from argparse import ArgumentParser
 from collections import Counter
 from operator import itemgetter
+from string import punctuation
+
+
+def process_line(line):
+    words = re.split('[\s—]+', line)  # Whitespace and dashes
+    return words
+
+
+def process_word(word):
+    word = word.strip(punctuation).lower()
+    return word
+
+
+punctuation = punctuation + '‘’“”'  # Add curly quotes
 
 if __name__ == '__main__':
     parser = ArgumentParser()
@@ -14,7 +29,9 @@ if __name__ == '__main__':
     count = Counter()
     with open(args.input_path) as file:
         for line in file:
-            count.update(line.split())
+            words = [process_word(word) for word in process_line(line)]
+            words = [word for word in words if word]
+            count.update(words)
 
     output_dir = os.path.dirname(args.output_path)
     if not os.path.exists(output_dir):
