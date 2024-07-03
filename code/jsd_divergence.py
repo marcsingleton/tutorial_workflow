c38f28a763267303aba462b6ff2ea0e224b4d561
Marc Singleton <marcsingleton@berkeley.edu>
2024-07-03 11:59:19 -0700

    Fixed calculation of jsd when reference probability is zero

--- code/jsd_divergence.py
+++ code/jsd_divergence.py
@@ -33,8 +33,8 @@ if __name__ == '__main__':
         p1 = counts1.get(word, 0) / n1
         p2 = counts2.get(word, 0) / n2
         m = 0.5 * (p1 + p2)
-        jsd += p1 * log(p1 / m)  # p1 as reference
-        jsd += p2 * log(p2 / m)  # p2 as reference
+        jsd += 0 if p1 == 0 else p1 * log(p1 / m)  # p1 as reference
+        jsd += 0 if p2 == 0 else p2 * log(p2 / m)  # p2 as reference
     jsd /= 2
 
     print(jsd, end='')
